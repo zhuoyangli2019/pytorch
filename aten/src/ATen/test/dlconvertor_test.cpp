a6641e6eacaca27b72c709bf750832bdb70a2c19
Brian Hirsh <hirsheybar@fb.com>
2020-08-24 13:09:27 -0700

    removed ws don't run deleter if it is null. issue #43166
    ghstack-source-id: 5de1d15f028244263b25a403f71fdacbb99a9338
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43271
    
    deleted unnecessary dlconvertor internals from test file
    
    ghstack-source-id: 5de1d15f028244263b25a403f71fdacbb99a9338
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43416

--- aten/src/ATen/test/dlconvertor_test.cpp
+++ aten/src/ATen/test/dlconvertor_test.cpp
@@ -30,3 +30,24 @@ TEST(TestDlconvertor, TestDlconvertorNoStrides) {
 
   ASSERT_TRUE(a.equal(b));
 }
+
+TEST(TestDlconvertor, TestDlconvertorNullDeleter) {
+  manual_seed(123);
+
+  Tensor a = rand({3, 4});
+
+  // no deleter necessary since dlManagedTensor is initialized on the stack
+  DLManagedTensor dlMTensor;
+  dlMTensor.deleter = nullptr;
+  dlMTensor.dl_tensor.data = a.data_ptr();
+  dlMTensor.dl_tensor.ctx = getDLContext(a, 0);
+  dlMTensor.dl_tensor.ndim = a.dim();
+  dlMTensor.dl_tensor.dtype = getDLDataType(a);
+  dlMTensor.dl_tensor.shape = const_cast<int64_t*>(a.sizes().data());
+  dlMTensor.dl_tensor.strides = const_cast<int64_t*>(a.strides().data());
+  dlMTensor.dl_tensor.byte_offset = 0;
+
+  Tensor b = fromDLPack(&dlMTensor);
+
+  ASSERT_TRUE(a.equal(b));
+}
