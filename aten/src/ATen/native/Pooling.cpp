53170c6f55773300312ef69b95721349ddf513f7
Heitor Schueroff <heitorschueroff@fb.com>
2020-08-26 13:46:14 -0700

    Implementation of max_pool2d without indices
    ghstack-source-id: cdfe7fd3082e8c62e2d86b38d6c15202450c3a0c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43267

--- aten/src/ATen/native/Pooling.cpp
+++ aten/src/ATen/native/Pooling.cpp
@@ -1,15 +1,8 @@
 #include <ATen/ATen.h>
-
-#include <ATen/NativeFunctions.h>
-#include <ATen/TensorUtils.h>
 #include <ATen/NamedTensorUtils.h>
-#include <ATen/native/xnnpack/Engine.h>
-#include <c10/macros/Macros.h>
-#include <c10/util/Exception.h>
 
-#include <tuple>
-
-namespace at { namespace native {
+namespace at {
+namespace native {
 
 static void check1d(
     const char* function_name,
@@ -17,29 +10,32 @@ static void check1d(
     IntArrayRef x) {
   TORCH_CHECK(
       x.size() == 1,
-      function_name, "() argument '", argument_name,
-      "' should contain one int (got ", x.size(), ")");
+      function_name,
+      "() argument '",
+      argument_name,
+      "' should contain one int (got ",
+      x.size(),
+      ")");
 }
 
-Tensor adaptive_avg_pool1d(const Tensor & self, IntArrayRef output_size) {
+Tensor adaptive_avg_pool1d(const Tensor& self, IntArrayRef output_size) {
   checkDim("adaptive_avg_pool1d", TensorArg(self, "self", 1), 3);
   check1d("adaptive_avg_pool1d", "output_size", output_size);
 
-  auto output = at::adaptive_avg_pool2d(
-      self.unsqueeze(2),
-      {1, output_size[0]});
+  auto output = at::adaptive_avg_pool2d(self.unsqueeze(2), {1, output_size[0]});
 
   return output.squeeze(2);
 }
 
-std::tuple<Tensor,Tensor> adaptive_max_pool1d(const Tensor & self, IntArrayRef output_size) {
+std::tuple<Tensor, Tensor> adaptive_max_pool1d(
+    const Tensor& self,
+    IntArrayRef output_size) {
   checkDim("adaptive_max_pool1d", TensorArg(self, "self", 1), 3);
   check1d("adaptive_max_pool1d", "output_size", output_size);
 
   Tensor output, indices;
-  std::tie(output, indices) = at::adaptive_max_pool2d(
-      self.unsqueeze(2),
-      {1, output_size[0]});
+  std::tie(output, indices) =
+      at::adaptive_max_pool2d(self.unsqueeze(2), {1, output_size[0]});
 
   return std::make_tuple(output.squeeze(2), indices.squeeze(2));
 }
@@ -71,7 +67,7 @@ std::tuple<Tensor, Tensor> max_pool1d_with_indices(
       {1, dilation[0]},
       ceil_mode);
 
-  output  = output.squeeze(2);
+  output = output.squeeze(2);
   indices = indices.squeeze(2);
 
   guard.reset();
@@ -107,46 +103,6 @@ Tensor avg_pool1d(
   return output.squeeze(2);
 }
 
-Tensor max_pool1d(
-    const Tensor& self,
-    IntArrayRef kernel_size,
-    IntArrayRef stride,
-    IntArrayRef padding,
-    IntArrayRef dilation,
-    bool ceil_mode) {
-  auto output_and_indices = at::max_pool1d_with_indices(
-      self, kernel_size, stride, padding, dilation, ceil_mode);
-  return std::get<0>(output_and_indices);
-}
-
-Tensor max_pool2d(
-    const Tensor& self,
-    IntArrayRef kernel_size,
-    IntArrayRef stride,
-    IntArrayRef padding,
-    IntArrayRef dilation,
-    bool ceil_mode) {
-  if (self.is_quantized()) {
-    return at::quantized_max_pool2d(self, kernel_size, stride, padding,
-                                    dilation, ceil_mode);
-  }
-  if (self.is_mkldnn()) {
-    return at::mkldnn_max_pool2d(
-        self, kernel_size, stride, padding, dilation, ceil_mode);
-  }
-
-#if defined(C10_MOBILE)
-  if(xnnpack::use_max_pool2d(self, kernel_size, padding, stride,
-                             dilation, ceil_mode)) {
-    return xnnpack::max_pool2d(
-        self, kernel_size, padding, stride, dilation, ceil_mode);
-  }
-#endif
-  auto output_and_indices = at::max_pool2d_with_indices(
-      self, kernel_size, stride, padding, dilation, ceil_mode);
-  return std::get<0>(output_and_indices);
-}
-
 Tensor max_pool3d(
     const Tensor& self,
     IntArrayRef kernel_size,
@@ -158,9 +114,19 @@ Tensor max_pool3d(
     return at::mkldnn_max_pool3d(
         self, kernel_size, stride, padding, dilation, ceil_mode);
   }
-  auto output_and_indices = at::max_pool3d_with_indices(
-      self, kernel_size, stride, padding, dilation, ceil_mode);
-  return std::get<0>(output_and_indices);
+  return std::get<0>(at::max_pool3d_with_indices(
+      self, kernel_size, stride, padding, dilation, ceil_mode));
+}
+
+Tensor max_pool1d(
+    const Tensor& self,
+    IntArrayRef kernel_size,
+    IntArrayRef stride,
+    IntArrayRef padding,
+    IntArrayRef dilation,
+    bool ceil_mode) {
+  return std::get<0>(at::max_pool1d_with_indices(
+      self, kernel_size, stride, padding, dilation, ceil_mode));
 }
 
 } // namespace native
